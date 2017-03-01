13556f76427a49c273eec13828cf67f2f4a16561
Durga E (dure) <dure@cisco.com>
2017-03-01 12:20:33 +0530

    Job_name1

--- ndscheduler/job.py
+++ ndscheduler/job.py
@@ -41,7 +41,7 @@ class JobBase:
 
     @classmethod
     def get_failed_description(cls):
-        job_name = utils.get_job_name(job_id)
+        job_name = utils.get_job_name(self.job_id)
         url_request.callurl(str(job_name) + " Failure")
         return utils.get_stacktrace()
 
@@ -49,7 +49,7 @@ class JobBase:
     def get_succeeded_description(cls):
         hostname = socket.gethostname()
         pid = os.getpid()
-        job_name = utils.get_job_name(job_id)
+        job_name = utils.get_job_name(self.job_id)
         url_request.callurl(str(job_name) + " Success")
         return 'hostname: %s | pid: %s' % (hostname, pid)
 
