401d6e273d11fab2af6411df5e97ec6aeb49d4c6
Durga E (dure) <dure@cisco.com>
2017-03-03 14:55:23 +0530

    Job ID changes5

--- ndscheduler/job.py
+++ ndscheduler/job.py
@@ -107,15 +107,16 @@ class JobBase:
             job.run(*args, **kwargs)
             datastore.update_execution(execution_id, state=constants.EXECUTION_STATUS_SUCCEEDED,
                                        description=cls.get_succeeded_description())
-            #job_name = utils.get_job_name(job_id)
-            url_request.callurl(str(job_id) + " Success")
+            
+            job_name = utils.get_job_name(job)
+            url_request.callurl(str(job_name) + " Success")
         except Exception as e:
             logger.exception(e)
             datastore.update_execution(execution_id,
                                        state=constants.EXECUTION_STATUS_FAILED,
                                        description=cls.get_failed_description())
-            #job_name = utils.get_job_name(job_id)
-            url_request.callurl(str(job_id) + " Failure")
+            job_name = utils.get_job_name(job)
+            url_request.callurl(str(job_name) + " Failure")
             
     def run(self, *args, **kwargs):
         """The "main" function for a job.
